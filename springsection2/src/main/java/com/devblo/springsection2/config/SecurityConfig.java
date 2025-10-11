876f51521bd632553bd5054e779c593f26bb28ae
blisah <sahinahmedblal48@gmail.com>
2025-10-11 23:32:55 +0300

    end of section2

---
+++ springsection2/src/main/java/com/devblo/springsection2/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.devblo.springsection2.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().denyAll());
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers("/myAccount",
+                        "/myBalance",
+                        "/myLoans",
+                        "/myLoans").authenticated()
+                .requestMatchers("/notices", "/contact","/error").permitAll());
+
+        http.formLogin(loginForm ->loginForm.disable() );
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
---
+++ springsection2/src/test/java/com/devblo/springsection2/BllBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devblo.springsection2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BllBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
